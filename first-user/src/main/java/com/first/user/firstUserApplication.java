716090c575c02dd06e5cd420c23df64d9a2270dd
CheeseGolem <lmx736338483@gmail.com>
2018-11-22 13:53:55 +0800

    maven-error

---
+++ first-user/src/main/java/com/first/user/firstUserApplication.java
@@ -0,0 +1,19 @@
+package com.first.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class firstUserApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(firstUserApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(firstUserApplication.class, args);
+	}
+}
